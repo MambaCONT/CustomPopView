26170ccf4b0e151743ebc6bbc7b077f6cd1e2f5f
BrainHe <hejinkun2015@gmail.com>
2023-04-06 18:24:18 +0800

    Update custom_pop.dart

--- custom_pop.dart
+++ custom_pop.dart
@@ -51,29 +51,3 @@ mixin CustomPopWidget {
     }
   }
 }
-
-extension BuildContextExt on BuildContext {
-  /// 获取当前组件的 RenderBox
-  RenderBox? renderBox() {
-    return this.findRenderObject() is RenderBox
-        ? (this.findRenderObject() as RenderBox)
-        : null;
-  }
-
-  /// 获取当前组件的 position
-  Offset? position({Offset offset = Offset.zero}) {
-    return this.renderBox()?.localToGlobal(offset);
-  }
-}
-
-extension GlobalKeyExt on GlobalKey {
-  /// 获取当前组件的 RenderBox
-  RenderBox? renderBox() => this.currentContext?.renderBox();
-
-  /// 获取当前组件的 position
-  Offset? position({Offset offset = Offset.zero}) =>
-      this.currentContext?.position(offset: offset);
-
-  /// 获取当前组件的 Size
-  Size? get size => this.currentContext?.size;
-}
